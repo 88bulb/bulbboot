237059e660ff317b7ffc2322787de7a7c3c3cfd5
matianfu <matianfu@gingerologist.com>
2021-12-09 05:15:09 +0800

    fix bug in node server

--- index.js
+++ index.js
@@ -3,11 +3,12 @@ const net = require('net')
 
 fs.readFile('hello-world.bin', (err, buf) => {
   if (err) return
+  const size = Buffer.alloc(4)
   const server = net.createServer(socket =>
     socket.on('data', data =>
       (data.toString() === 'BULBBOOT\n')
         ? socket.end(Buffer.concat([
-          Buffer.alloc(4).writeUInt32LE(buf.length),
+          (size.writeUInt32LE(buf.length), size),
           buf]))
         : socket.end())
   ).listen({ host: '10.42.0.1', port: 6016 },
