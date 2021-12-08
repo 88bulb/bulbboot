ffc05f7df041c23b0708f6309e6651048bd6f7fd
matianfu <matianfu@gingerologist.com>
2021-12-09 04:57:54 +0800

    add hello-world as test firmware

--- index.js
+++ index.js
@@ -1,35 +1,15 @@
-const path = require('path');
-const fs = require('fs');
-const net = require('net');
+const fs = require('fs')
+const net = require('net')
 
-
-const server = net.createServer((socket) => {
-    socket.on('data', data => {
-        console.log(data.toString())
-        const filepath = './hello-world.bin'
-        fs.stat(filepath, (err, stats) => {
-            if (err) {
-                socket.end();     
-            } else {
-                let buf = Buffer.alloc(4)
-                buf.writeUInt32LE(stats.size)
-                console.log(buf)
-                socket.write(buf, () => {
-                    fs.createReadStream('./hello-world.bin').pipe(socket)
-                })
-            } 
-        })
-        
-    });
-}).on('error', (err) => {
-  // Handle errors here.
-  throw err;
-});
-
-// Grab an arbitrary unused port.
-server.listen({
-    host: '10.42.0.1',
-    port: 6016,
-}, () => {
-  console.log('opened server on', server.address());
-});
+fs.readFile('hello-world.bin', (err, buf) => {
+  if (err) return
+  const server = net.createServer(socket =>
+    socket.on('data', data =>
+      (data.toString() === 'BULBBOOT\n')
+        ? socket.end(Buffer.concat([
+          Buffer.alloc(4).writeUInt32LE(buf.length),
+          buf]))
+        : socket.end())
+  ).listen({ host: '10.42.0.1', port: 6016 },
+    () => console.log(server.address()))
+})
