c905a6d150073b06dd1b3902c29fad1a0d0cfe95
matianfu <matianfu@gingerologist.com>
2022-01-04 15:47:01 +0800

    include merge_bin.sh

--- merge_bin.sh
+++ merge_bin.sh
@@ -4,6 +4,7 @@ esptool.py \
     -o bulbboot_merged.bin \
     --flash_mode dio \
     --flash_size 4MB \
-    0x1000 build/bootloader/bootloader.bin \
-    0x8000 build/partition_table/partition-table.bin \
+    0x00000 build/bootloader/bootloader.bin \
+    0x08000 build/partition_table/partition-table.bin \
+    0x0d000 build/ota_data_initial.bin \
     0x10000 build/bulbboot.bin
